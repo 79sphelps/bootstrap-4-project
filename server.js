a525ee60f5ea9119ec35bc95333855deb75439f7
Steve Phelps <79sphelps@gmail.com>
2018-08-08 07:56:41 -0700

    Updating path to static files in server.js

--- server.js
+++ server.js
@@ -5,18 +5,18 @@
  |--------------------------------------
  */
 
- // Modules
- const express = require('express');
- const path = require('path');
- const bodyParser = require('body-parser');
- const mongoose = require('mongoose');
- const methodOverride = require('method-override');
- const cors = require('cors');
+// Modules
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const cors = require("cors");
 
- // Config
- const config = require('./server/config');
+// Config
+const config = require("./server/config");
 
- // server.js
+// server.js
 /*
  |--------------------------------------
  | Dependencies
@@ -27,12 +27,16 @@ console.log(config.MONGO_URI);
 mongoose.connect(config.MONGO_URI);
 const monDb = mongoose.connection;
 
-monDb.on('error', function() {
-    console.log('MongoDB Connection Error. Please make sure that', config.MONGO_URI, 'is running');
+monDb.on("error", function() {
+  console.log(
+    "MongoDB Connection Error. Please make sure that",
+    config.MONGO_URI,
+    "is running"
+  );
 });
 
-monDb.once('open', function callback() {
-    console.info('Connected to MongoDB:', config.MONGO_URI);
+monDb.once("open", function callback() {
+  console.info("Connected to MongoDB:", config.MONGO_URI);
 });
 
 /*
@@ -45,17 +49,18 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride("X-HTTP-Method-Override"));
 app.use(cors());
 
 // Set port
-const port = process.env.PORT || '8083';
-app.set('port', port);
+const port = process.env.PORT || "8083";
+app.set("port", port);
 
 // Set static path to Angular app in dist
 // Don't run in dev
-if (process.env.NODE_ENV !== 'dev') {
-    app.use('/', express.static(path.join(__dirname, './dist')));
+if (process.env.NODE_ENV !== "dev") {
+  // app.use('/', express.static(path.join(__dirname, './dist')));
+  app.use("/", express.static(path.join("./dist")));
 }
 
 /*
@@ -64,14 +69,14 @@ if (process.env.NODE_ENV !== 'dev') {
  |--------------------------------------
  */
 
-require('./server/api')(app, config);
+require("./server/api")(app, config);
 
 // Pass routing to Angular app
 // Don't run in dev
-if (process.env.NODE_ENV !== 'dev') {
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/dist/index.html'));
-    });
+if (process.env.NODE_ENV !== "dev") {
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/dist/index.html"));
+  });
 }
 
 /*
@@ -80,4 +85,4 @@ if (process.env.NODE_ENV !== 'dev') {
  |--------------------------------------
  */
 
- app.listen(port, () => console.log(`Server running on localhost:${port}`));
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
